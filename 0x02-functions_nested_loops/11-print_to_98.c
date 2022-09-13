ef8f2f4314711591e97fc2126ab40e4a95d04c4b
Samuel Ian <osamwelian3@gmail.com>
2022-09-13 02:41:33 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,29 +1,40 @@
-
-
-/*
- * File: 11-print_to_98.c
- * Auth: Michael Ndungu Kamotho
- */
-
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
- * 11-print_to_98.c - prints all natural numbers from n to 98 followed by a line
- * @c: The character to be checked.
- *
- * Return: 1 if character is lowercase, 0 otherwise.
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number prints to 98, whether < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
  */
-
-void print_to_98(int n){
-	int count;
-	int number = n;
-
-	for(count = n; count <99; count++){
-	   printf(number);
- 	   number++;
-	   printf(',');
-
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
 	}
-	printf('\n');
-
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
