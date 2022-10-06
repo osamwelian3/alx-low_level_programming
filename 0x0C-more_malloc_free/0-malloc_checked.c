7ff47f1c0e94c8595e7711bec38d6d40b870819d
Samuel Ian <osamwelian3@gmail.com>
2022-10-06 21:21:12 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * malloc_checked - Prints the number of argumnets
- *
- * @b: argument count
- *
- * Return: return 0
- */
+ *malloc_checked - allocate memory.
+ *@b: size of the memory to be allocated.
+ *Return: pointer;
+ **/
+
 void *malloc_checked(unsigned int b)
 {
-	char *i;
+	char *p;
 
-	i = malloc(b);
-	if (i == NULL)
+	p = malloc(b);
+	if (p == NULL)
+	{
 		exit(98);
-	return (i);
+		return (NULL);
+	}
+	return (p);
 }
